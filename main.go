ef2783a5f452e88fc21c40fdc9f7ab4831ba64b1
SuInk <xilei08004@qq.com>
2023-02-16 01:02:47 +0800

    update message handle bug

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ func init() {
 }
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("panic:", err)
+			return
+		}
+	}()
 	go cqhttp.Run()
 	for {
 		time.Sleep(5 * time.Second)
